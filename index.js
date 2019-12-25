ec44facc66e7f5c8bdca34f0469f8dd205357837
wilson <jwmrf@cin.ufpe.br>
2019-12-25 06:38:05 -0300

    Initial commit api

---
+++ index.js
@@ -0,0 +1,7 @@
+async function str_multi_replace(texto, array_to, from){
+    for(let to of array_to){
+        texto = texto.split(to).join(from)
+    }
+    return texto;
+}
+module.export = str_multi_replace;
